5bc267d011e6a11e15a1ebd560ec1ebd6235f732
eawendtjr <eawendtjr@hotmail.com>
2016-04-13 23:13:07 -0600

    Update DualMC33926MotorShield.h

--- DualMC33926MotorShield/DualMC33926MotorShield.h
+++ DualMC33926MotorShield/DualMC33926MotorShield.h
@@ -13,20 +13,20 @@ class DualMC33926MotorShield
                            unsigned char nD2, unsigned char nSF); // User-defined pin selection. 
     
     // PUBLIC METHODS
-    void init(); // Initialize TIMER 1, set the PWM to 20kHZ. 
+    void init(); // Initialize TIMER 2, set the PWM to 3.92kHZ. 
     void setM1Speed(int speed); // Set speed for M1.
     void setM2Speed(int speed); // Set speed for M2.
     void setSpeeds(int m1Speed, int m2Speed); // Set speed for both M1 and M2.
-    unsigned int getM1CurrentMilliamps(); // Get current reading for M1. 
-    unsigned int getM2CurrentMilliamps(); // Get current reading for M2.
+    //unsigned int getM1CurrentMilliamps(); // Get current reading for M1. 
+    //unsigned int getM2CurrentMilliamps(); // Get current reading for M2.
     unsigned char getFault(); // Get fault reading.
     
   private:
     unsigned char _nD2;
     unsigned char _M1DIR;
     unsigned char _M2DIR;
-    static const unsigned char _M1PWM = 9;
-    static const unsigned char _M2PWM = 10;
+    static const unsigned char _M1PWM = 3;
+    static const unsigned char _M2PWM = 11;
     unsigned char _nSF;
     unsigned char _M1FB;
     unsigned char _M2FB;
